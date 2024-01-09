41ded015ce2676143a4db25e86846c030adf73ce
lapis33 <142216213+lapis33@users.noreply.github.com>
2024-01-09 22:08:36 +0800

    Add initialization task shell

--- src/tasks/init.rs
+++ src/tasks/init.rs
@@ -1,4 +1,5 @@
-use crate::uart_println;
+use crate::{drivers::uart::read_char, uart_print, uart_println};
+use alloc::{string::String, vec::Vec};
 
 const LOGO: &str = r#"
                /$$                    
@@ -16,4 +17,22 @@ const LOGO: &str = r#"
 
 pub async fn main() {
     uart_println!("{}", LOGO);
+
+    loop {
+        uart_print!("[root@pinc] ");
+
+        let mut input = String::new();
+
+        loop {
+            let character = read_char();
+            uart_print!("{}", character);
+
+            match character {
+                '\n' => break,
+                other => input.push(other),
+            }
+        }
+
+        let arguments: Vec<&str> = input.split_ascii_whitespace().collect();
+    }
 }
